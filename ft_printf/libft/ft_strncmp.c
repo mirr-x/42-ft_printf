a86b9a215c1f046a9cd5f4af956c70c8c8245cbf
mirr <121735620+mirr-x@users.noreply.github.com>
2025-11-12 13:54:47 +0000

    Add string comparison and search functions: ft_strncmp, ft_strnstr, and ft_strrchr

---
+++ ft_printf/libft/ft_strncmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molahrac <molahrac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 20:08:22 by molahrac          #+#    #+#             */
+/*   Updated: 2025/11/02 12:53:37 by molahrac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * @n : is limit of comparition  we will compare n of the s1 and s2
+ */
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	if (n == i)
+		return (0);
+	else
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
---
+++ ft_printf/libft/ft_strnstr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molahrac <molahrac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 01:05:12 by molahrac          #+#    #+#             */
+/*   Updated: 2025/11/02 12:53:47 by molahrac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * Locates the first occurrence of 'needle' in len 'haystack' within 'len' bytes.
+ *  Returns a pointer to the start or NULL if not found
+ */
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!haystack && len == 0)
+		return (NULL);
+	if (needle[0] == '\0')
+		return ((char *) haystack);
+	while (haystack[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && i + j < len)
+		{
+			if (needle[j + 1] == '\0')
+				return ((char *) &haystack[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}

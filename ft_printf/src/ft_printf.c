66b0a4fab3132d02e1c489dd4ef9b8faaf87b781
mirr <121735620+mirr-x@users.noreply.github.com>
2025-11-12 13:54:23 +0000

    Implement core functionality for ft_printf, including formatted output and helper functions

---
+++ ft_printf/src/ft_printf.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molahrac <molahrac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/06 06:08:53 by molahrac          #+#    #+#             */
+/*   Updated: 2025/11/12 13:35:47 by molahrac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_printf.h"
+
+void	get_correct_format_parameter(va_list args, char c, size_t *cn)
+{
+	if (c == 'c')
+		*cn += ft_putchar_fd(va_arg(args, int), 1);
+	else if (c == 's')
+		*cn += ft_putstr_fd(va_arg(args, char *), 1);
+	else if (c == 'p')
+		*cn += ft_puthex((size_t)va_arg(args, void *), 1);
+	else
+		*cn += ft_putchar_fd(c, 1);
+}
+
+int	ft_printf(const char *s, ...)
+{
+	va_list	args;
+	size_t	i;
+	size_t	cn;
+
+	if (!s)
+		return (-1);
+	va_start(args, s);
+	cn = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '%')
+		{
+			i++;
+			get_correct_format_parameter(args, s[i++], &cn);
+			continue ;
+		}
+		ft_putchar_fd(s[i++], 1);
+		cn++;
+	}
+	va_end(args);
+	return (cn);
+}
